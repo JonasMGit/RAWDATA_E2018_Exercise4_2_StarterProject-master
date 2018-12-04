34519af2c6caebfc336494ab3e371069799e8ad9
jonas <jonas@LAPTOP-MSH4DBM6>
2018-12-04 13:40:19 +0100

    Add project files.

---
+++ Exercise4_2/wwwroot/js/app/postviewModel.js
@@ -0,0 +1,26 @@
+﻿define(["jquery", "knockout"], function ($, ko) {
+
+    var posts = ko.observableArray([]);
+
+   /* var next = function () {
+
+
+    }*/
+    //hello
+
+  
+    
+    //loads data into posts array
+     $.getJSON("api/posts", function (data) {
+        posts(data.items);
+    });
+
+    return {
+        
+        posts
+       
+    };
+
+
+});
+
---
+++ Exercise4_2/wwwroot/js/main.js
@@ -0,0 +1,13 @@
+﻿requirejs.config({
+    baseUrl: "js",
+    paths: {
+        jquery: "lib/jQuery/dist/jquery.min",
+        knockout: "lib/knockout/dist/knockout.debug",
+        text: "lib/text/text",
+        dataService: "DataAccessLayer/DataService.cs"
+    }
+});
+
+require(['knockout', 'app/postviewModel'], function (ko, vm) {
+    ko.applyBindings(vm);
+});
